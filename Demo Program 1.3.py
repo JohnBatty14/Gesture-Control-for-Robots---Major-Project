b0ba8b87c23ee63f9bc9d6909a988f38c18c7f62
JohnBatty14 <JohnBatty14@users.noreply.github.com>
2016-03-14 15:02:31 +0000

    Update Demo Program 1.3.py

--- Demo Program 1.3.py
+++ Demo Program 1.3.py
@@ -5,16 +5,26 @@ cap = cv2.VideoCapture(0)
 
 if cap.isOpened() == False:
     cap.open()
-
-def printBlobLoc(keypoints):
-    for n in range(0, keypoints.size()):
-        print "X Coord: %d" % (keypoints[n].pt.x)
-        print "Y Coord: %d" % (keypoints[n].pt.y)
-
-
-def findBlobs(frame):
+    
+def findWhiteBlobs(frame):
     params = cv2.SimpleBlobDetector_Params()
-    params.filterByColor = True
+    params.filterByColor = 1
+    params.blobColor = 255
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.7
+    params.filterByConvexity = True
+    params.minConvexity = 0.8
+    params.filterByCircularity = True
+    params.minCircularity = 0.8
+    params.filterByArea = True
+    #params.minArea = 900
+    detector = cv2.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(frame)
+    return keypoints
+
+def findBlackBlobs(frame):
+    params = cv2.SimpleBlobDetector_Params()
+    params.filterByColor = 1
     params.blobColor = 0
     params.filterByInertia = True
     params.minInertiaRatio = 0.7
@@ -26,18 +36,22 @@ def findBlobs(frame):
     params.minArea = 900
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(frame)
-    #printBlobLoc(keypoints)
     return keypoints
 
 while(True):
     ret, frame = cap.read()
 
     # Stuff that we do to the frame goes here
-    foundKeypoints = findBlobs(frame)   
-    foundBlobs = cv2.drawKeypoints(frame, foundKeypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    cv2.imshow('Keypoints', foundBlobs)
-    # Display the resulting frame
+    foundBKeypoints = findBlackBlobs(frame)
+    foundWKeypoints = findWhiteBlobs(frame)
+
+    foundWBlobs = cv2.drawKeypoints(frame, foundWKeypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    foundBBlobs = cv2.drawKeypoints(frame, foundBKeypoints, np.array([]), (0,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     
+    
+    cv2.imshow('White keypoints', foundWBlobs)
+    cv2.imshow('Black keypoints', foundBBlobs)
+    # Display the resulting frame
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
